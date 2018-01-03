ccc6c6aa0b576832665872fc8b6645b947363cfe
Joseph Cheek <joseph@ziquid.com>
2018-01-03 18:10:34 -0500

    more intro to USLCE

--- sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
@@ -6,6 +6,7 @@
 <style>
   body {
     margin: 6px auto;
+    width: 100%;
   }
   p {
     margin: 6px;
@@ -14,6 +15,8 @@
 
   <p>Uprising: St. Louis Community Edition</p>
   <p>January 3, 2018 Alpha release 0.0.1</p>
+  <h4>Please note this release is for testing only</h4>
+  <h4>Do not expect anything in this release to be permanent</h4>
   <form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
     <input type="Submit" value="Continue to the game"/>
   </form>
