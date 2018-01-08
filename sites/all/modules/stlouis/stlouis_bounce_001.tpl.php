1725b91b80373ba411479283e46c2bc312fb9e28
Joseph Cheek <joseph@ziquid.com>
2018-01-08 17:11:53 -0500

    really added date this time

--- sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
@@ -17,7 +17,7 @@
   <p>January 3, 2018 Alpha release 0.0.1</p>
   <h4>Please note this release is for testing only</h4>
   <h4>Do not expect anything in this release to be permanent</h4>
-  <p>Working to fix Luck purchases</p>
+  <p>Jan 8: Working to fix Luck purchases</p>
   <form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
     <input type="Submit" value="Continue to the game"/>
   </form>
