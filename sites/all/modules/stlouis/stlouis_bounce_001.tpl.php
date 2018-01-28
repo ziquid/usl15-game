5c594ed3c1847950eaa0a1f39b37511ed7c74342
Joseph Cheek <joseph@ziquid.com>
2018-01-27 20:54:18 -0500

    added clan rules

--- sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_001.tpl.php
@@ -17,6 +17,7 @@
   <p>January 3, 2018 Alpha release 0.0.1</p>
   <h4>Please note this release is for testing only</h4>
   <h4>Do not expect anything in this release to be permanent</h4>
+  <p>Jan 27: Fixed clan list, added clan rules</p>
   <p>Jan 24: Fixed referral codes</p>
   <p>Jan 9: Some Galaxy S8 devices have the home page and map screen off a little.
   We've fixed it some but let us know if your device still has the home page or
