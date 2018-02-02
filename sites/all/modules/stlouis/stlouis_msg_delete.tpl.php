9e55e83946240aee8841fc35973c55615405a40c
Joseph Cheek <joseph@ziquid.com>
2018-02-01 23:14:02 -0500

    fix litecoin, add 'pruner' competency

--- sites/all/modules/stlouis/stlouis_msg_delete.tpl.php
+++ sites/all/modules/stlouis/stlouis_msg_delete.tpl.php
@@ -1,13 +1,39 @@
 <?php
 
-  global $game, $phone_id;
+/**
+ * @file stlouis_msg_delete.tpl.php
+ * Delete a message sent to you.
+ *
+ * Synced with CG: yes
+ * Synced with 2114: no
+ */
 
-  $fetch_user = '_' . arg(0) . '_fetch_user';
-  $fetch_header = '_' . arg(0) . '_header';
+global $game, $phone_id;
 
-  $game_user = $fetch_user();
-  include_once(drupal_get_path('module', $game) . '/game_defs.inc');
- 
-  $sql = 'delete from user_messages where id = %d;';
-  $result = db_query($sql, $msg_id);
-  drupal_goto($game . '/user/' . check_plain(arg(2)));
+$fetch_user = '_' . arg(0) . '_fetch_user';
+$fetch_header = '_' . arg(0) . '_header';
+$game_user = $fetch_user();
+include(drupal_get_path('module', $game) . '/game_defs.inc');
+$arg2 = check_plain(arg(2));
+
+// Check permissions.
+$sql = 'select fkey_users_to_id
+  from user_messages
+  where id = %d;';
+$result = db_query($sql, $msg_id);
+$msg = db_fetch_object($result);
+
+// Not recipient of msg?
+if ($msg->fkey_users_to_id != $game_user->id) {
+// FIXME jwc 10Apr2014 -- deduct karma
+  db_set_active('default');
+  drupal_goto($game . '/home/' . $arg2);
+}
+
+competency_gain($game_user, 'pruner');
+
+$sql = 'delete from user_messages where id = %d;';
+db_query($sql, $msg_id);
+
+db_set_active('default');
+drupal_goto($game . '/user/' . $arg2);
