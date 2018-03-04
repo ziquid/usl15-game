2ab2ae3544d8f8f3ceb2eec431c6feccfc0bfb6f
Joseph Cheek <joseph@ziquid.com>
2018-03-03 23:17:14 -0500

    scale to device width, needed for wkwebview on iOS

--- sites/all/themes/cdc_games/page.tpl.php
+++ sites/all/themes/cdc_games/page.tpl.php
@@ -6,6 +6,7 @@
   <title><?php print $head_title ?></title>
   <?php print $styles ?>
   <?php print $scripts ?>
+  <meta name="viewport" content="width=device-width, user-scalable=no">
 </head>
 
 <body <?php print theme("onload_attribute"); ?>>
