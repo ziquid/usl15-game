ed011f9623ac78f90612fbd34ce0741891699db5
Joseph Cheek <joseph@ziquid.com>
2018-03-31 07:40:49 +0800

    security updates, bug fixes.

--- sites/all/modules/stlouis/stlouis_bounce_002.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_002.tpl.php
@@ -23,6 +23,7 @@
 <form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
   <input type="Submit" value="Continue to the game"/>
 </form>
+<p>31 Mar: Security updates, bug fixes.</p>
 <p>30 Mar: You can now &quot;work&quot; a job every 8 hours to gain extra
   competency and income for that job.</p>
 <p>27 Mar: Jobs now have competencies attached to them.  Every day you play you
