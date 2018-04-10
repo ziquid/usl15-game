294f2a75ed8442a17edff822f7df8da39268a9ac
Joseph Cheek <joseph@ziquid.com>
2018-04-10 09:43:26 -0400

    added russian hackers

--- sites/all/modules/stlouis/stlouis_bounce_002.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_002.tpl.php
@@ -15,52 +15,5 @@
 
   <p>Uprising: St. Louis, Community Edition</p>
   <p>March 6, 2018 Beta release 0.2.0</p>
-  <h4>Please note this release is for testing only</h4>
-  <h4>Some things may change</h4>
-  <h4>All accounts have been reset!</h4>
-  <h4>If you bought Luck during the Alpha testing, email zipport@ziquid.com
-    to have our support staff re-credit it to your account</h4>
-<form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
-  <input type="Submit" value="Continue to the game"/>
-</form>
-<p>9 Apr: Update to balance the Feel the Pulse of the City mission.</p>
-<p>6 Apr: Added Disaffected Youth and Egging.</p>
-<p>6 Apr: Fixed "Can't Sell" button and Savings Account, lowered PNG money req't to 25 per level.</p>
-<p>3 Apr: Added Canadian Dollar, Money Market Account to invest in.</p>
-<p>31 Mar: Added Carpenter and Lyft driver jobs.</p>
-<p>31 Mar: Security updates, bug fixes.</p>
-<p>30 Mar: You can now &quot;work&quot; a job every 8 hours to gain extra
-  competency and income for that job.</p>
-<p>27 Mar: Jobs now have competencies attached to them.  Every day you play you
-  will increase your competency for that job.</p>
-<p>27 Mar: More clan actions.</p>
-<p>26 Mar: More quests.</p>
-<p>24 Mar: Support for Android 8.</p>
-<p>23 Mar: Added Bicycle for getting around faster.</p>
-<p>23 Mar: Added iPhone and iPad for initiative and endurance.</p>
-<p>23 Mar: Added levels 78 and 79.</p>
-<p>22 Mar: Added button for 700 Luck ($99.99).</p>
-<p>21 Mar: Opened Vandeventer for elections.</p>
-<p>21 Mar: Increased the drop percentage of loot for the Hood Missions.&nbsp; Added another mission.</p>
-<p>21 Mar: Fixed a bug in calculating opponent's debate influence.</p>
-<p>18 Mar: The new Debate Coach will critique your last three debates,
-  giving insight into your debate performance.</p>
-<p>17 Mar: Happy St. Patrick's Day!</p>
-<p>17 Mar: New equipment, Android phone, for purchase gives you 2 initiative.</p>
-<p>17 Mar: New equipment, Android tablet, for purchase gives you 3 endurance.</p>
-<p>17 Mar: Added flower seeds and the ability to plant flowers.</p>
-<p>16 Mar: New investment: Savings Account.</p>
-<p>14 Mar: New agent for loot: Art critic.</p>
-<p>14 Mar: New action: Give to a clan member.</p>
-<p>14 Mar: New job: Handy-person.</p>
-<p>14 Mar: Lowered max amount of currency to gain/lose per debate to 1/10 of hourly income.</p>
-<p>14 Mar: Wait for competencies to occur lowered to 3 minutes (from 5).</p>
-<p>13 Mar: Show chance of loot for quests.</p>
-<p>13 Mar: Made the names of hoods on the map easier to read.</p>
-<p>12 Mar: Opened two new hoods for elections.</p>
-<p>12 Mar: Fixed the Write on a Billboard action.</p>
-<p>11 Mar: Changed debate timeout to 15 minutes instead of 20.</p>
-<p>11 Mar: Fixed an issue where some colors were not showing up on the map.</p>
-<form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
-  <input type="Submit" value="Continue to the game"/>
-</form>
+<h4>The game has now reached been updated</h4>
+<h4>Please update your client</h4>
