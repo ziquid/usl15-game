7d0fd891a7099cfd8ca65504be928ed7dc758042
Joseph Cheek <joseph@ziquid.com>
2018-04-17 16:40:51 -0400

    announce updated downtown mission.

--- sites/all/modules/stlouis/stlouis_bounce_030.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_030.tpl.php
@@ -20,6 +20,7 @@
 <form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
   <input type="Submit" value="Continue to the game"/>
 </form>
+<p>17 Apr: Updated downtown mission and loot.</p>
 <p>16 Apr: Added conference room for larger clans.</p>
 <p>16 Apr: Added levels 80-83, balanced level requirements for offices.</p>
 <p>16 Apr: Soulard's Market is now is Soulard!  Adopt a Pet is now in Cheltenham.</p>
