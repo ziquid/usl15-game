36a83ecb774b58ded487463ba1b63693a07a9015
Susan Cheek <susan.e.cheek@gmail.com>
2018-04-18 15:28:17 -0400

    stlouis-2-include3 deleted parenthasis

--- sites/all/modules/stlouis/stlouis_msg_delete.tpl.php
+++ sites/all/modules/stlouis/stlouis_msg_delete.tpl.php
@@ -13,7 +13,7 @@ global $game, $phone_id;
 $fetch_user = '_' . arg(0) . '_fetch_user';
 $fetch_header = '_' . arg(0) . '_header';
 $game_user = $fetch_user();
-include(drupal_get_path('module', $game) . '/game_defs.inc');
+include drupal_get_path('module', $game) . '/game_defs.inc';
 $arg2 = check_plain(arg(2));
 
 // Check permissions.
