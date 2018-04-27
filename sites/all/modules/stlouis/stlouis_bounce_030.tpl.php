dbdf46043776344a86bf15c340c39eb420f10b1f
Joseph Cheek <joseph@ziquid.com>
2018-04-27 19:44:06 -0400

    announce clear clan annoucements, erase your wall

--- sites/all/modules/stlouis/stlouis_bounce_030.tpl.php
+++ sites/all/modules/stlouis/stlouis_bounce_030.tpl.php
@@ -20,6 +20,7 @@
 <form method=post action="/<?php echo $game; ?>/home/<?php echo $arg2;?>">
   <input type="Submit" value="Continue to the game"/>
 </form>
+<p>27 Apr: Added Clear Clan Announcements and Erase Your Wall actions.</p>
 <p>27 Apr: Added Teenage Girls with associated action and competency.</p>
 <p>27 Apr: Added some Forest Park missions, removed Forum link from home page, and added level 86.</p>
 <p>24 Apr: Campaigning mission has a second-round bonus and associated action.</p>
