48d49a12ec42451653795ac86a10b16bb3153369
Joseph Cheek <joseph@ziquid.com>
2018-05-02 01:38:47 -0400

    log luck use

--- sites/all/modules/stlouis/stlouis_elders_do_fill.tpl.php
+++ sites/all/modules/stlouis/stlouis_elders_do_fill.tpl.php
@@ -1,147 +1,157 @@
 <?php
 
-  global $game, $phone_id;
+/**
+ * @file stlouis_elders_do_fill.tpl.php
+ * Fill your stats by using Luck.
+ *
+ * Synced with CG: yes
+ * Synced with 2114: no
+ */
 
-  $fetch_user = '_' . arg(0) . '_fetch_user';
-  $fetch_header = '_' . arg(0) . '_header';
+global $game, $phone_id;
 
-  $game_user = $fetch_user();
-  include drupal_get_path('module', $game) . '/game_defs.inc';
+$fetch_user = '_' . $game . '_fetch_user';
+$fetch_header = '_' . $game . '_header';
+
+$game_user = $fetch_user();
+include drupal_get_path('module', $game) . '/game_defs.inc';
 
 /*  if ($game == 'stlouis') {
-  	
-  	$link = $destination ? $destination : "/$game/user/$phone_id";
-  	
-  	$fetch_header($game_user);
 
-  	echo <<< EOF
+  $link = $destination ? $destination : "/$game/user/$phone_id";
+
+  $fetch_header($game_user);
+
+  echo <<< EOF
 <div class="title">
-  Luck-free 4th
+Luck-free 4th
 </div>
 <div class="subtitle">
-  Sorry!&nbsp; No $luck today!
+Sorry!&nbsp; No $luck today!
 </div>
 <div class="subtitle">
-  <a href="$link">
-    <img src="/sites/default/files/images/{$game}_continue.png"/>
-  </a>
+<a href="$link">
+  <img src="/sites/default/files/images/{$game}_continue.png"/>
+</a>
 </div>
 EOF;
 
-  	db_set_active('default');
-    
-    return;
-    
-  }*/
+  db_set_active('default');
 
-  $quest_lower = strtolower($quest);
-  $experience_lower = strtolower($experience);
-  
-  switch ($fill_type) {
-    
-    case 'action':
-    	
-/*    	if (($game == 'stlouis') && 
-    		($game_user->actions < $game_user->actions_max)) {
-    			
-      	$sql = 'update users set actions = actions_max where id = %d;';
-	      $result = db_query($sql, $game_user->id);
+  return;
 
-	      $game_user = $fetch_user();
-	     	$fetch_header($game_user);
+}*/
 
-        echo '<div class="subtitle">Amusez-vous bien !</div>';
-        echo '<div class="subtitle">
-				  <a href="/' . $game . '/home/' . $phone_id . '">
-    				<img src="/sites/default/files/images/' . $game . '_continue.png"/>
-  				</a>
-				</div>';
-    
-        db_set_active('default');
-        return;
-	      
-    	}
-*/  	
-      if ($game_user->luck < 1) {
-        
-        $fetch_header($game_user);
+$quest_lower = strtolower($quest);
+$experience_lower = strtolower($experience);
+$amount_filled = 0;
+$luck_remaining = $game_user->luck;
+$sql = 'insert into {luck_use} (fkey_users_id, use_type, amount_filled, luck_remaining)
+  values (%d, "%s", %d, %d);';
 
-        echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
-          . '</div>';
-        echo '<div class="try-an-election-wrapper"><div
-          class="try-an-election"><a href="/' . $game .
-          '/elders_ask_purchase/' . $phone_id .
-          '">Purchase more ' . $luck . '</div></div>';
-    
-        db_set_active('default');
-        return;
-        
-      }
-      
-      if ($game_user->actions < $game_user->actions_max) {
-        $sql = 'update users set actions = actions_max, luck = luck - 1
-          where id = %d;';
-        $result = db_query($sql, $game_user->id);
-      }
-      
-      break;
-      
-    case 'energy':
-      
-      if ($game_user->luck < 1) {
-        
-        $fetch_header($game_user);
+switch ($fill_type) {
 
-        echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
-          . '</div>';
-        echo '<div class="try-an-election-wrapper"><div
-          class="try-an-election"><a href="/' . $game .
-          '/elders_ask_purchase/' . $phone_id .
-          '">Purchase more ' . $luck . '</div></div>';
-    
-        db_set_active('default');
-        return;
-        
-      }
+  case 'action':
 
-      if ($game_user->energy < $game_user->energy_max) {
-      
-        $sql = 'update users set energy = energy_max, luck = luck - 1
-          where id = %d;';
-        $result = db_query($sql, $game_user->id);
-        
-      }
-      
-      break;
+/*    	if (($game == 'stlouis') &&
+      ($game_user->actions < $game_user->actions_max)) {
 
-    case 'money':
-      
-      if ($game_user->luck < 1) {
-        
-        $fetch_header($game_user);
+      $sql = 'update users set actions = actions_max where id = %d;';
+      $result = db_query($sql, $game_user->id);
 
-        echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
-          . '</div>';
-        echo '<div class="try-an-election-wrapper"><div
-          class="try-an-election"><a href="/' . $game .
-          '/elders_ask_purchase/' . $phone_id .
-          '">Purchase more ' . $luck . '</div></div>';
-    
-        db_set_active('default');
-        return;
-        
-      }
+      $game_user = $fetch_user();
+      $fetch_header($game_user);
 
-      $offer = ($game_user->income - $game_user->expenses) * 5;
-      $offer = min($offer, $game_user->level * 1000);
-      $offer = max($offer, $game_user->level * 100);
-      
-      $sql = 'update users set money = money + %d, luck = luck - 1
+      echo '<div class="subtitle">Amusez-vous bien !</div>';
+      echo '<div class="subtitle">
+        <a href="/' . $game . '/home/' . $phone_id . '">
+          <img src="/sites/default/files/images/' . $game . '_continue.png"/>
+        </a>
+      </div>';
+
+      db_set_active('default');
+      return;
+
+    }
+*/
+    if ($game_user->luck < 1) {
+      $fetch_header($game_user);
+
+      echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
+        . '</div>';
+      echo '<div class="try-an-election-wrapper"><div
+        class="try-an-election"><a href="/' . $game .
+        '/elders_ask_purchase/' . $phone_id .
+        '">Purchase more ' . $luck . '</div></div>';
+      db_query($sql, $game_user->id, $fill_type, $amount_filled, $luck_remaining);
+      db_set_active('default');
+      return;
+    }
+
+    if ($game_user->actions < $game_user->actions_max) {
+      $sql = 'update users set actions = actions_max, luck = luck - 1
         where id = %d;';
-      $result = db_query($sql, $offer, $game_user->id);
-        
-      break;
-      
-  }
-    
-  drupal_goto($game . '/user/' . $phone_id);
+      db_query($sql, $game_user->id);
+      $amount_filled = $game_user->actions_max;
+      $luck_remaining -= 1;
+    }
+
+    break;
+
+  case 'energy':
+
+    if ($game_user->luck < 1) {
+      $fetch_header($game_user);
+
+      echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
+        . '</div>';
+      echo '<div class="try-an-election-wrapper"><div
+        class="try-an-election"><a href="/' . $game .
+        '/elders_ask_purchase/' . $phone_id .
+        '">Purchase more ' . $luck . '</div></div>';
+      db_query($sql, $game_user->id, $fill_type, $amount_filled, $luck_remaining);
+      db_set_active('default');
+      return;
+    }
+
+    if ($game_user->energy < $game_user->energy_max) {
+      $sql = 'update users set energy = energy_max, luck = luck - 1
+        where id = %d;';
+      db_query($sql, $game_user->id);
+      $amount_filled = $game_user->energy_max;
+      $luck_remaining -= 1;
+    }
+
+    break;
+
+  case 'money':
+
+    if ($game_user->luck < 1) {
+      $fetch_header($game_user);
+
+      echo '<div class="land-failed">' . t('Out of @s!', array('@s' => $luck))
+        . '</div>';
+      echo '<div class="try-an-election-wrapper"><div
+        class="try-an-election"><a href="/' . $game .
+        '/elders_ask_purchase/' . $phone_id .
+        '">Purchase more ' . $luck . '</div></div>';
+      db_query($sql, $game_user->id, $fill_type, $amount_filled, $luck_remaining);
+      db_set_active('default');
+      return;
+    }
+
+    $offer = ($game_user->income - $game_user->expenses) * 5;
+    $offer = min($offer, $game_user->level * 1000);
+    $offer = max($offer, $game_user->level * 100);
+
+    $sql = 'update users set money = money + %d, luck = luck - 1
+      where id = %d;';
+    db_query($sql, $offer, $game_user->id);
+    $amount_filled = $offer;
+    $luck_remaining -= 1;
+    break;
+
+}
+
+db_query($sql, $game_user->id, $fill_type, $amount_filled, $luck_remaining);
+drupal_goto($game . '/user/' . $phone_id);
