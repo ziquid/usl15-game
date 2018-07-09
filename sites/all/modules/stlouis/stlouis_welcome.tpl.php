911b29853c66dc6a2bd779ae3424794e60744412
Joseph Cheek <joseph@ziquid.com>
2018-07-09 00:39:42 -0400

    fix(?) creation of toxibots

--- sites/all/modules/stlouis/stlouis_welcome.tpl.php
+++ sites/all/modules/stlouis/stlouis_welcome.tpl.php
@@ -22,8 +22,8 @@ $item = db_fetch_object($result);
 
 // Allow multiple from my IP.
 if (($item->count > 5) && ($ip_address != '69.255.171.73') &&
-  ($ip_address != '73.82.211.0')) {
-  echo 'Error E-2242: ' . $arg2 .
+  ($ip_address != '73.82.211.0') && $ip_address != '127.0.0.1') {
+  echo 'Error E-2242: ' . $arg2 . ' from ' . $ip_address .
     '.  Please email <strong>zipport@ziquid.com</strong>.';
   exit();
 }
