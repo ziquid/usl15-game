6dd4b37e4ea9aefd47351f65b14ec7a8678f9f07
Dennis Abasa <denzabasa@yahoo.com.ph>
2018-08-25 08:17:29 +0800

    Remove extra new line.

--- sites/all/modules/stlouis/stlouis_staff_hire.tpl.php
+++ sites/all/modules/stlouis/stlouis_staff_hire.tpl.php
@@ -13,7 +13,6 @@ global $game, $phone_id;
 // ------ CONTROLLER ------
 include drupal_get_path('module', $game) . '/game_defs.inc';
 $game_user = $fetch_user();
-
 $title = t('Hire @staff and @agents', [
   '@staff' => $game_text['staff'],
   '@agents' => $game_text['agents'],
