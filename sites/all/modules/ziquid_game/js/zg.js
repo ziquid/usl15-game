67ae3b7b2280cfab9a2035521651e5c93a3dac32
Joseph Cheek <joseph@ziquid.com>
2019-03-15 15:18:53 +0800

    colored background is the default

--- sites/all/modules/ziquid_game/js/zg.js
+++ sites/all/modules/ziquid_game/js/zg.js
@@ -6,21 +6,22 @@ Drupal.behaviors.zg = function (context) {
     }
 
     if (Drupal.settings.zg.enabled_alpha) {
-      var level = parseInt(Drupal.settings.zg.level);
-      // level = 1;
-      var red = Math.max(level - 100, 0);
-      var green = Math.max(Math.floor(100 - level), 0);
-      var blue = level;
-      if (blue > 100) {
-        blue = 200 - blue;
-      }
-      red = Math.floor(red * 0.6);
-      green = Math.floor(green * 0.6);
-      blue = Math.floor(blue * 0.6);
       $('body').addClass('alpha');
-      $('body.alpha').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
-      $('.background-color').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
     }
+
+    var level = parseInt(Drupal.settings.zg.level);
+    // level = 1;
+    var red = Math.max(level - 100, 0);
+    var green = Math.max(Math.floor(100 - level), 0);
+    var blue = level;
+    if (blue > 100) {
+      blue = 200 - blue;
+    }
+    red = Math.floor(red * 0.6);
+    green = Math.floor(green * 0.6);
+    blue = Math.floor(blue * 0.6);
+    $('body.game-stlouis').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
+    $('.background-color').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue + ')');
   }
 
   jQuery(".fit-box").each(function() {
