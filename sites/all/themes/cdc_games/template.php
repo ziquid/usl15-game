4b8ac0aba29dc16617b8e53fa189fef52c9c2705
Joseph Cheek <joseph@ziquid.com>
2019-10-05 21:20:34 +0800

    add support for USL_ESA teaser, fix a few minor layout/theming issues.

--- sites/all/themes/cdc_games/template.php
+++ sites/all/themes/cdc_games/template.php
@@ -11,7 +11,29 @@ function cdc_games_placeholder($text) {
  * Implements template_preprocess_page.
  */
 function cdc_games_preprocess_page(&$vars) {
-  $vars['body_classes'] = str_replace('page-stlouis',
-    'game-' . arg(0) . ' page-' . arg(1),
+  $game = $arg0 = drupal_html_class(arg(0));
+  $page = $arg1 = drupal_html_class(arg(1));
+
+  // Pretend usl_esa is stlouis for now.
+  if ($game == 'usl-esa') {
+    $game = 'stlouis';
+  }
+
+  $vars['body_classes'] = str_replace('page-' . $arg0,
+    'game-' . $game . ' page-' . $page,
     $vars['body_classes']);
+}
+
+// Backported from Drupal 7.
+if (!function_exists('drupal_html_class')) {
+  function drupal_html_class($class) {
+
+    // The output of this function will never change, so this uses a normal
+    // static instead of drupal_static().
+    static $classes = [];
+    if (!isset($classes[$class])) {
+      $classes[$class] = drupal_clean_css_identifier(drupal_strtolower($class));
+    }
+    return $classes[$class];
+  }
 }
