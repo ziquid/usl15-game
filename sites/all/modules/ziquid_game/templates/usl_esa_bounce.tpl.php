7b52799bdb1f89c45aae2cb43be8e682a90db865
Joseph Cheek <jcheek@users.noreply.github.com>
2020-04-26 16:27:55 -0400

    more landscape fixes

--- sites/all/modules/ziquid_game/templates/usl_esa_bounce.tpl.php
+++ sites/all/modules/ziquid_game/templates/usl_esa_bounce.tpl.php
@@ -29,7 +29,7 @@ $game_user = db_fetch_object($result);
 firep($game_user, 'game_user object');
 
 if ($game_user->meta == 'admin' || $game_user->meta == 'employee') {
-  $button = zg_render_button('/stlouis/home') . "<p class='center'>(if you dare)</p>";
+  $button = zg_render_button('/stlouis/bounce') . "<p class='center'>(if you dare)</p>";
 }
 else {
   $button = '';
