7a52e8a001f3445a5d5d1cd80af2fcedbabf4787
Marjory Del Rosario <marjory@ziquid.com>
2020-04-30 21:46:55 +0800

    username-elders-bug: fixed the coding

--- sites/all/modules/ziquid_game/templates/zg_elders.tpl.php
+++ sites/all/modules/ziquid_game/templates/zg_elders.tpl.php
@@ -19,16 +19,15 @@
 
 /* ------ CONTROLLER ------ */
 
-global $game, $phone_id;
-include drupal_get_path('module', 'zg') . '/includes/' . $game . '_defs.inc';
-$game_user = zg_fetch_user();
-zg_fetch_header($game_user);
-
 if (empty($game_user->username) || $game_user->username == '(new player)') {
   db_set_active();
   drupal_goto($game . '/choose_name/' . $arg2);
 }
 
+global $game, $phone_id;
+include drupal_get_path('module', 'zg') . '/includes/' . $game . '_defs.inc';
+$game_user = zg_fetch_user();
+zg_fetch_header($game_user);
 
 if (substr($phone_id, 0, 3) == 'ai-') {
 
