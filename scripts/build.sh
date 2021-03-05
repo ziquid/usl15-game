0577f13f57f74fab083ad30dc0538ef10aa84324
Marjory Del Rosario <marjory@ziquid.com>
2021-03-06 04:19:02 +0800

    created scripts

---
+++ scripts/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# build uslce on OVH3 or local
+
+set -xe
+DIRNAME=$(dirname "$0")
+BASENAME=$(basename "$0")
+FULLNAME="$DIRNAME/$BASENAME"
+
+# Linux/non-apache?  sudo to apache
+[ $(uname) == Linux ] && [ $(whoami) != apache ] && exec sudo su -l apache -s /bin/bash "$FULLNAME" "$@"
+
+function update() {
+  drush cr -l $1 || :
+  drush updb -y -l $1
+  drush cr -l $1 || :
+  [ $(uname) == Linux ] && drush cim -y sync -l $1 || drush cim -y sync --partial -l $1
+  drush cr -l $1
+  drush cc views -l $1
+}
+
+# main()
+cd $(dirname "$0")/..
+git fetch
+
+if [ "$1" != "" ]; then
+  echo checking out branch $1...
+  git co $1
+  git pull
+fi
+
+composer.phar install
+
+update uslce
+
+scripts/db-dump.sh
---
+++ scripts/db-dump.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# dump a (sanitized?) copy of the db
+
+# shellcheck disable=SC2112
+function do_dump_san() {
+
+  echo
+  echo Dumping and sanitizing the $1 db...
+  echo
+  set -x
+
+  # dump the full production db
+  drush sql-dump -l $1 > $1.sql
+
+  # sanitize the full production db
+  drush -q sqlsan --sanitize-email=email+%uid@example.com --sanitize-password=no -y -l $1
+
+  # dump the sanitized version
+  drush sql-dump -l $1 > $1.san.sql
+
+  # reload the production db and remove the dump on the file system
+  drush sqlc -l $1 < $1.sql
+  shred -u $1.sql || rm -P $1.sql || rm $1.sql
+
+  # move the sanitized version to public files and create compressed and downloadable copies
+  # (drupal's .htaccess auto-blocks .sql files)
+  # also store a backup somewhere
+  gzip -c $1.san.sql > web/sites/$1/files/$1.san.sql.gz
+
+  if [ -d /var/backup ]; then
+    BACKUP_FILE=$1.san.sql.gz-$(date | sed -e 's,[: ],-,g')
+    cp web/sites/$1/files/$1.san.sql.gz /var/backup/$BACKUP_FILE
+    ln -sf /var/backup/$BACKUP_FILE /var/backup/$1.latest.sql.gz
+  else
+    echo no /var/backup dir exists!
+  fi
+
+  mv $1.san.sql web/sites/$1/files/$1.san.sql.ok-to-download
+
+  set +x
+}
+
+function do_dump() {
+
+  echo
+  echo Dumping the $1 db...
+  echo
+  set -x
+
+  # dump the full production db
+  drush sql-dump -l $1 > $1.sql
+  gzip -f $1.sql
+
+  set +x
+}
+
+[ $(uname) == Linux ] && DUMP=do_dump_san || DUMP=do_dump
+$DUMP ulsce
