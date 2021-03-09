8c3529659925b69199a02499d2e06610e113adec
Marjory Del Rosario <marjory@ziquid.com>
2021-03-09 08:40:04 +0800

    added scripts

--- sites/all/themes/cdc_games/page.tpl.php
+++ sites/all/themes/cdc_games/page.tpl.php
@@ -2,6 +2,13 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8KR7H4');</script>
+<!-- End Google Tag Manager -->
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
@@ -10,7 +17,10 @@
 </head>
 
 <body <?php print theme("onload_attribute"); ?> class="<?php print $body_classes; ?>">
-<?php print $google_tag_body; ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8KR7H4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php if ($messages != ""): ?>
   <div id="message"><?php print $messages ?></div>
 <?php endif; ?>
