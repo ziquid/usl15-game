d5abcb16c87b77d4390432363d1d7fa030a49763
Joseph Cheek <joseph@ziquid.com>
2023-05-02 15:54:14 -0600

    adding controls to see if that works.

--- sites/all/modules/ziquid_game/templates/zg_elders.tpl.php
+++ sites/all/modules/ziquid_game/templates/zg_elders.tpl.php
@@ -59,7 +59,7 @@ list($offer, $comment) = zg_luck_money_offer($game_user);
 $offer = number_format($offer);
 $elder_welcome = '<div class="speech-bubble-wrapper background-color">
   <div class="wise_old_man with-video ' . $happy . '">
-    <video id="wise-old-man" autoplay loop muted poster="/sites/default/files/images/stlouis_wise_old_man_resting.jpg">
+    <video id="wise-old-man" autoplay loop muted controls poster="/sites/default/files/images/stlouis_wise_old_man_resting.jpg">
       <source src="/sites/default/files/videos/stlouis-handbrake.mp4" type="video/mp4">
       <!--<source src="/sites/default/files/videos/stlouis_wise_old_man_resting.mp4" type="video/mp4">-->
     </video>
